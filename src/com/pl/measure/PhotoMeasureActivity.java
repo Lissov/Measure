1c558f05d9019ba9c6232a73d6216ec06739895d
lissov <pavlo.lissov@gmail.com>
2014-02-10 21:55:48 +0100

    PhotoMeasure small fix

--- src/com/pl/measure/PhotoMeasureActivity.java
+++ src/com/pl/measure/PhotoMeasureActivity.java
@@ -6,6 +6,11 @@ import android.widget.*;
 import android.content.pm.*;
 import android.content.*;
 import android.provider.*;
+import android.graphics.*;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import android.net.*;
 
 public class PhotoMeasureActivity extends Activity
 {
@@ -24,11 +29,69 @@ public class PhotoMeasureActivity extends Activity
 	}
 	
 	static final int REQUEST_PHOTO = 1;
+	private static String photoFileName;
 	
 	private void makePhoto(){
 		Intent photoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		if (photoIntent.resolveActivity(getPackageManager()) != null){
-			startActivityForResult(photoIntent, REQUEST_PHOTO);
+			File photoFile = null;
+			try{
+				photoFile = createPhotoFile();
+			} catch (IOException exc){
+				Toast.makeText(this, exc.getMessage(), Toast.LENGTH_LONG).show();
+			}
+			if (photoFile != null){
+				photoFileName = photoFile.getAbsolutePath();
+				photoIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+				startActivityForResult(photoIntent, REQUEST_PHOTO);	
+			}
 		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
+		if (requestCode == REQUEST_PHOTO && resultCode == RESULT_OK){
+			ImageView image = (ImageView)findViewById(R.id.ivPhoto);
+			Bundle extras = data.getExtras();
+			Bitmap imageBitmap = (Bitmap)extras.get("data");
+			image.setImageBitmap(imageBitmap);
+		}
+	}
+	
+	private void setPic() {
+		ImageView image = (ImageView)findViewById(R.id.ivPhoto);
+		
+		// Get the dimensions of the View
+		int targetW = image.getWidth();
+		int targetH = image.getHeight();
+
+		// Get the dimensions of the bitmap
+		BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+		bmOptions.inJustDecodeBounds = true;
+		BitmapFactory.decodeFile(photoFileName, bmOptions);
+		int photoW = bmOptions.outWidth;
+		int photoH = bmOptions.outHeight;
+
+		// Determine how much to scale down the image
+		int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+		// Decode the image file into a Bitmap sized to fill the View
+		bmOptions.inJustDecodeBounds = false;
+		bmOptions.inSampleSize = scaleFactor;
+		bmOptions.inPurgeable = true;
+
+		Bitmap bitmap = BitmapFactory.decodeFile(photoFileName, bmOptions);
+		image.setImageBitmap(bitmap);
+	}
+	
+	private File createPhotoFile() throws IOException
+	{
+		String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+		String filename = "jpg_" + timestamp;
+		File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File image= File.createTempFile(filename, ".jpg", storageDir);
+		
+		return image;
 	}
 }
